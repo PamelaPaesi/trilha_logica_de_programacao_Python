c48c6f3baa920d4b933b7fc077d2a00b9483a2fc
Pâmela Bieger <pamela.bieger@e-unicred.com.br>
2025-01-21 11:33:14 -0300

    [Desafio 9] Divisão de Valores

---
+++ divisao_valores.py
@@ -0,0 +1,18 @@
+def validaOpcao(posicao):
+    while True:
+        try:
+            return float(input('Insira o ' + posicao + ' número: '))
+        except ValueError:
+            print("Valor inválido!")
+
+x = validaOpcao("primeiro")
+while True:
+    y = validaOpcao("segundo")
+    if y == 0:
+        print('Valor inválido!')
+    else:
+        break
+    
+
+remainder = x % y
+print(remainder)
